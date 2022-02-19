c22d0a028f8bd164e75bcf948add9d22ddad2fe6
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-02-19 16:53:55 +0530

    Time: 63 ms (17.89%), Space: 14 MB (87.29%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSymmetric(self, root):
+        return self.checkNodes(root.left, root.right)
+
+    def checkNodes(self, leftNode, rightNode):
+        if leftNode == None and rightNode == None:
+            result = True
+        elif leftNode == None or rightNode == None:
+            result = False
+        else:
+            if leftNode.val != rightNode.val:
+                result = False
+            else:
+                result = self.checkNodes(leftNode.left, rightNode.right)
+                if result:
+                    result = self.checkNodes(leftNode.right, rightNode.left)
+        return result
