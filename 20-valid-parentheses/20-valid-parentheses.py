4f00f0d74edcfe1edd6d3396ee35625423b464d4
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-03-12 23:10:33 +0530

    Time: 47 ms (46.54%), Space: 13.9 MB (82.45%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValid(self, s):
+        arr=[]
+        dit={']':'[','}':'{',')':'('}
+        for i in s:
+            if i in dit.keys():
+                top=arr.pop() if arr else ""
+                if top!=dit[i]:
+                    return False
+            else:
+                arr.append(i)
+        return not arr 
