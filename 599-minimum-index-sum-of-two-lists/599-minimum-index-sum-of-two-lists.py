fb73fb0519c5771e00ab4d875ae7f4108fdcf748
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-04-20 23:02:39 +0530

    Time: 378 ms (20.15%), Space: 14.5 MB (9.88%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
@@ -4,15 +4,11 @@ class Solution:
         for i in list1:
             if i in list2:
                 dit[i]=list1.index(i)+list2.index(i)
-        dit={
-            k:v for k,v in sorted(dit.items(),key=lambda item:item[1])
-        }
+
         x=min(dit.values())
         arr=[]
         for i,j in dit.items():
             if j==x:
                 arr.append(i)
-            elif j>x:
-                return arr
         return arr
         
