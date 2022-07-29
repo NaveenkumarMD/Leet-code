444ebcfb050362d7f3540778fb2318aa70edebed
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-07-29 18:41:01 +0530

    Time: 64 ms (20.98%), Space: 13.8 MB (60.39%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -1,15 +1,31 @@
-from collections import Counter
+from collections import defaultdict
 class Solution:
-    def find(self,arr):
-        dit=Counter(arr)
-        for i in dit:
-            if dit[i]>1:
-                return False
-        return True
     def countGoodSubstrings(self, s: str) -> int:
         count=0
-        for i in range(len(s)-3+1):
-            if self.find(s[i:i+3]):
-                count+=1
+        dit=defaultdict(lambda:0)
+        x=0
+        for i in s[:3]:
+            if dit[i]==0:
+                x+=1
+                dit[i]=1
+            else:
+                dit[i]+=1
+        if x==3:
+            count+=1
+        for i in range(1,len(s)-3+1):
+            c=s[i-1]
+            d=s[i+3-1]
+            dit[c]-=1
+            if dit[c]==0:
+                x-=1
+            if dit[d]==0:
+                x+=1
+            dit[d]+=1
+            if x==3:count+=1
         return count
-        
+                
+                
+            
+                
+                
+            
