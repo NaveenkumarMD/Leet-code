d98981ea36de00743ea07c047ce0181fccd21f9e
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-08-03 20:57:42 +0530

    Time: 647 ms (91.63%), Space: 13.9 MB (60.50%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,30 +1,20 @@
-class Solution(object):
-	def longestPalindrome(self, s):
-		"""
-		:type s: str
-		:rtype: str
-		"""
-		n = len(s)
-		i = 0
-		substring = ""
-		while i < n:
-			l = i
-			r = i
-			while l >= 0 and r < n and s[l] == s[r]:
-				l -= 1
-				r += 1
-			l += 1 #pointers are moved out of bound before breaking the while loop, so we move them back
-			r -= 1
-			if r - l + 1 > len(substring):
-				substring = s[l: r + 1]
-			l = i
-			r = i + 1
-			while l >= 0 and r < n and s[l] == s[r]:
-				l -= 1
-				r += 1
-			l += 1
-			r -= 1
-			if r - l + 1 > len(substring):
-				substring = s[l: r + 1]
-			i += 1
-		return substring
+class Solution(object): 
+    def longestPalindrome(self, s):
+        res=""
+        for i in range(len(s)):
+            start=i
+            end=i
+            while start>=0 and end<=len(s)-1 and s[start]==s[end]:
+                start-=1
+                end+=1
+            if len(res)<len(s[start+1:end]):
+                res=s[start+1:end]
+            start=i
+            end=i+1
+            while start>=0 and end<=len(s)-1 and s[start]==s[end]:
+                start-=1
+                end+=1
+            if len(res)<len(s[start+1:end]):
+                res=s[start+1:end]
+
+        return res
