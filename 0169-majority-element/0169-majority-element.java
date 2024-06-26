39a1c6f94b4fb733e88e280aa794f6e78bb6cc7d
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2024-06-26 09:39:06 +0530

    Time: 7 ms (39.05%), Space: 52.8 MB (59.41%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,26 @@
 import java.util.*;
 public class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
-        for(int i:nums){
-            if(h.get(i)!=null){
-                h.put(i,h.get(i)+1);
+ public int majorityElement(int[] nums) {
+        // Arrays.sort(nums);
+        Arrays.sort(nums); // Sort the array
+        
+        int maxCount = 1; // Initial max count of elements is at least 1
+        int count = 1; // Start counting from the first element
+        int majorityElement = nums[0]; // Initialize majority element
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++; // Increment the count if the same element repeats
+            } else {
+                count = 1; // Reset count if a different element is found
             }
-            else{
-                h.put(i,1);
+            
+            if (count > maxCount) {
+                maxCount = count; // Update max count
+                majorityElement = nums[i]; // Update majority element
             }
         }
-        System.out.println(h);
-        int i=-1;
-        int key=0;
-        for(Map.Entry<Integer,Integer> m : h.entrySet()){
-            if(m.getValue()>i){
-                i=m.getValue();
-                key=m.getKey();
-                
-            }
-        }
-    return key;
+        
+        return majorityElement;
     }
 }
